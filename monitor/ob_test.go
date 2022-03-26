66bdb5c9fcf8953c77fee476fe2003bc9a9f8228
Hank Shen <swh@admpub.com>
2022-03-26 17:10:08 +0800

    update

---
+++ monitor/ob_test.go
@@ -0,0 +1,19 @@
+package monitor
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDefer(t *testing.T) {
+	defer func() {
+		fmt.Println("============1")
+	}()
+	defer func() {
+		fmt.Println("============2")
+	}()
+	//panic("~")
+	//output:
+	//============2
+	//============1
+}
